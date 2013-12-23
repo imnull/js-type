2f9bc0c8854895d28cd74bd76d0a35cf12978774
Ma Ke <mk31415926535@gmail.com>
2013-12-23 22:00:36 +0800

    js-type for nodejs

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ js-type
 =======
 
 JavaScript Type Getter
+
+# Lite type-string
+
+    console.log( 'lite-type: ', jstype.lite());
+    console.log( 'lite-type: ', jstype.lite(null));
+    console.log( 'lite-type: ', jstype.lite({}));
+    console.log( 'lite-type: ', jstype.lite('{}'));
+    console.log( 'lite-type: ', jstype.lite(1));
+    console.log( 'lite-type: ', jstype.lite(true));
+    console.log( 'lite-type: ', jstype.lite(/a/));
+    console.log( 'lite-type: ', jstype.lite([]));
+    console.log( 'lite-type: ', jstype.lite(function(){}));
+    console.log( 'lite-type: ', jstype.lite(window));
+
+Null's type-string is 'null'
+
+# Type Judge
+   
+   console.log( jstype.isArray( [ ] ) );
+   console.log( jstype.isObject( { } ) );
+   console.log( jstype.isString( '' ) );
+   //...
---
+++ js-type-node.js
@@ -0,0 +1,40 @@
+(function(){
+
+function ostr(val){	return Object.prototype.toString.call(val); }
+function tstr(val){	return typeof(val); }
+
+var foo = {
+	isString : function(val){ return tstr(val) == 'string'; }
+	, isObject : function(val){ return ostr(val) == '[object Object]'; }
+	, isArray : function(val){ return ostr(val) == '[object Array]'; }
+	, isRegExp : function(val){ return ostr(val) == '[object RegExp]'; }
+	, isFunction : function(val){ return tstr(val) == 'function'; }
+	, isBoolean : function(val){ return tstr(val) == 'boolean'; }
+	, isNumber : function(val){ return tstr(val) == 'number'; }
+	, isNull : function(val){ return val === null; }
+	, isUndefined : function(val){ return tstr(val) == 'undefined'; }
+	, lite : function(val){
+		var s = ostr(val);
+		if(foo.isUndefined(val)){
+			return 'undefined';
+		} else if(foo.isNull(val)){
+			return 'null';
+		} else if(foo.isArray(val)){
+			return 'array';
+		} else if(foo.isObject(val)){
+			return 'object';
+		} else if(foo.isRegExp(val)){
+			return 'regexp';
+		} else if(typeof(val) == 'object') {
+			return 'unknown';
+		} else {
+			return tstr(val);
+		}
+	}
+}
+
+
+
+module.exports = foo;
+
+})()
